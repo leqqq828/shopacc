744c3b2a4e56c82e2e9725156c631fef5d35627c
leqqq828 <lewiskirven@gmail.com>
2026-07-26 21:13:36 +0700

    Add Vietnamese authentication messages

---
+++ backend/lang/vi.js
@@ -0,0 +1,14 @@
+module.exports.auth_message = {
+    na_acc_too_short: 'Tên tài khoản phải có tối thiểu 5 ký tự',
+    na_acc_too_long: 'Tên tài khoản chỉ được phép có tối đa 50 ký tự',
+    na_acc_type: 'Tên tài khoản không được phép chứa ký tự đặc biệt',
+    password_length: "Mật khẩu phải có từ 5 - 50 ký tự",
+    password_type: "Mật khẩu không được chứa ký tự đặc biệt",
+    phone_number_invalid: "Số điện thoại bạn vừa nhập không chính xác!",
+    name_account_existed: "Tên tài khoản đã tồn tại, vui lòng sử dụng tên khác!",
+    create_success: "Tạo tài khoản thành công. Bây giờ bạn có thể đăng nhập vào tài khoản của mình",
+    unknown_error: "Đã có lỗi không xác định xảy ra, nếu lỗi này còn tiếp tục vui lòng liên hệ admin",
+    phone_number_existed: "Số điện thoại đã được đăng ký!",
+
+    login_invalid: "Tài khoản hoặc mật khẩu không đúng xin vui lòng kiểm tra lại."
+}
